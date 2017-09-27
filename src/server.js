e2f0db5c0e27f409d633c0b414d6f9dde74175f1
godie007 <diegof.e3@gmail.com>
2017-09-27 12:58:00 -0500

    updated database connection

--- src/server.js
+++ src/server.js
@@ -4,19 +4,34 @@ const mongoose = require('mongoose');
 const expressGraphQL = require('express-graphql');
 const bodyParser = require('body-parser');
 const schema = require('./schema/schema');
-
-
-
+var tunnel = require('tunnel-ssh');
 const app = express();
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/plaza-delta')
-mongoose.connection.once('open',()=>console.log('Conexion Correcta!')).on('error',error=>console.log(error))
+var config = {
+  username:'heydev',
+  host:'35.184.198.215',
+  privateKey:require('fs').readFileSync('/home/godie007/.ssh/heydev'),
+  port:22,
+  dstPort:27017,
+  Password:'',
+  localHost:'127.0.0.1',
+  localPort: 27017
+};
 
-app.use(bodyParser.json());
-app.use('/graphql', expressGraphQL({
-  schema,
-  graphiql: true
-}));
+  tunnel(config, function (error, server) {
 
+  if(error){
+      console.log("SSH connection error: " + error);
+  }
+  console.log("Tiner conectado!" + server);
+   mongoose.Promise = global.Promise;
+   mongoose.connect('mongodb://localhost/plaza-delta')
+   mongoose.connection.once('open',()=>console.log('Conexion Correcta!')).on('error',error=>console.log(error))
 
+   app.use(bodyParser.json());
+   app.use('/graphql', expressGraphQL({
+     schema,
+     graphiql: true
+  }));
+
+});
 module.exports = app;
