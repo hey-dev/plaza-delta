a5ee9bbb330692f95e84b6bb3e704a1735a59f02
godie007 <diegof.e3@gmail.com>
2017-10-02 14:30:28 -0500

    fix  conflicts

--- src/schema/root_query_type.js
+++ src/schema/root_query_type.js
@@ -2,14 +2,9 @@ module.exports = `
   # Root
   type Query {
     users: [User]
-<<<<<<< HEAD
-  }
-`;
-=======
     user(id: String): User
     establishment(id: String): Establishment
     establishments: [Establishment]
     feedback(id: String): Feedback
   } 
 `
->>>>>>> godie007/create-schema-types
