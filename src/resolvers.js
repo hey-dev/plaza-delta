e21ecc3f9975f8a6e24b91364e8f6be913b24928
Alejandro Baez <nativo.alejandro@gmail.com>
2017-10-02 17:26:45 -0500

    remove unused packages :fire:

--- src/resolvers.js
+++ src/resolvers.js
@@ -1,15 +1,12 @@
-const mongoose = require('mongoose');
-const Account = mongoose.model('account');
-const Attendant = mongoose.model('attendant');
-const Establishment = mongoose.model('establishment');
-const Feedback = mongoose.model('feedback');
-const Order = mongoose.model('order');
-const Product = mongoose.model('product');
-const User = mongoose.model('user');
-const ShoppingCart = mongoose.model('shoppingCart');
-const _ = require('lodash');
-
-
+const mongoose = require("mongoose");
+const Account = mongoose.model("account");
+const Attendant = mongoose.model("attendant");
+const Establishment = mongoose.model("establishment");
+const Feedback = mongoose.model("feedback");
+const Order = mongoose.model("order");
+const Product = mongoose.model("product");
+const User = mongoose.model("user");
+const ShoppingCart = mongoose.model("shoppingCart");
 
 const resolvers = {
   Query: {
@@ -31,27 +28,31 @@ const resolvers = {
   },
   Mutation: {
     createUser: (_, { account, user }) =>
-      Account.create(account).then(account => {
-        user['account'] = account.id;
-        return User.create(user);
-      }).catch(err => console.log(err)),
+      Account.create(account)
+        .then(account => {
+          user["account"] = account.id;
+          return User.create(user);
+        })
+        .catch(err => console.log(err)),
 
     createEstablishment: (_, { attendant, establishment }) =>
-      Attendant.create(attendant).then(attendant => {
-        establishment['attendant'] = attendant.id;
-        return Establishment.create(establishment);
-      }).catch(err => console.log(err)),
+      Attendant.create(attendant)
+        .then(attendant => {
+          establishment["attendant"] = attendant.id;
+          return Establishment.create(establishment);
+        })
+        .catch(err => console.log(err)),
 
-      createFeedback: (_, { feedback }) => Feedback.create(feedback),
-      createProduct: (_, { product }) => Product.create(product),
-      createOrder: (_, { order,shoppingCart }) => 
-        Order.create(order).then(order => {
-          shoppingCart.forEach(cart => {
-            cart['order'] = order.id;
-            ShoppingCart.create(cart);
-          })
-          return order;
-        }),
+    createFeedback: (_, { feedback }) => Feedback.create(feedback),
+    createProduct: (_, { product }) => Product.create(product),
+    createOrder: (_, { order, shoppingCart }) =>
+      Order.create(order).then(order => {
+        shoppingCart.forEach(cart => {
+          cart["order"] = order.id;
+          ShoppingCart.create(cart);
+        });
+        return order;
+      }).catch(err => console.log(err))
   }
 };
 
--- src/server.js
+++ src/server.js
@@ -1,29 +1,28 @@
-require('./models');
-
-const bodyParser = require('body-parser');
-const express = require('express');
-const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
-const fs = require('fs');
-const mongoose = require('mongoose');
-const tunnel = require('tunnel-ssh');
-
-const schema = require('./schema/schema');
-const resolvers = require('./resolvers');
-
+require("./models");
+const bodyParser = require("body-parser");
+const express = require("express");
+const { graphqlExpress, graphiqlExpress } = require("apollo-server-express");
+const mongoose = require("mongoose");
+const schema = require("./schema/schema");
+const resolvers = require("./resolvers");
 const app = express();
-  
+
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/plaza-delta', {
-  useMongoClient: true,
+mongoose.connect("mongodb://localhost/plaza-delta", {
+  useMongoClient: true
 });
 mongoose.connection
-  .once('open', () => console.log('ʕ·ᴥ·ʔ connected to MongoDB'))
-  .on('error', err => console.log(err));
+  .once("open", () => console.log("ʕ·ᴥ·ʔ connected to MongoDB"))
+  .on("error", err => console.log(err));
 
-app.use('/graphql', bodyParser.json(), graphqlExpress({
-  schema,
-  resolvers
-}));
-app.get('/graphiql', graphiqlExpress({ endpointURL: '/graphql' }));
+app.use(
+  "/graphql",
+  bodyParser.json(),
+  graphqlExpress({
+    schema,
+    resolvers
+  })
+);
+app.get("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
 
 module.exports = app;
